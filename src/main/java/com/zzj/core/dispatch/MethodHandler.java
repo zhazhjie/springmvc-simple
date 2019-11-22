2457962824a9729b4c0919b10afb6979ea83ed26
zhazhjie <583714409@qq.com>
2019-11-22 23:34:03 +0800

    结构调整

--- src/main/java/com/zzj/core/dispatch/MethodHandler.java
+++ src/main/java/com/zzj/core/dispatch/MethodHandler.java
@@ -2,18 +2,54 @@ package com.zzj.core.dispatch;
 
 import com.zzj.core.constant.RequestMethod;
 import com.zzj.core.constant.ResponseType;
-import lombok.Getter;
-import lombok.Setter;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
-@Setter
-@Getter
+
 public class MethodHandler {
     private RequestMethod[] requestMethod;
     private ResponseType responseType;
     private Object instance;
     private Method method;
     private String path;
+
+    public RequestMethod[] getRequestMethod() {
+        return requestMethod;
+    }
+
+    public void setRequestMethod(RequestMethod[] requestMethod) {
+        this.requestMethod = requestMethod;
+    }
+
+    public ResponseType getResponseType() {
+        return responseType;
+    }
+
+    public void setResponseType(ResponseType responseType) {
+        this.responseType = responseType;
+    }
+
+    public Object getInstance() {
+        return instance;
+    }
+
+    public void setInstance(Object instance) {
+        this.instance = instance;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public void setMethod(Method method) {
+        this.method = method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
