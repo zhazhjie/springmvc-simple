8b638a36a3769de421d55bc3d6f2c60ab10dbb12
zhazhjie <583714409@qq.com>
2019-12-04 17:16:34 +0800

    Controller异常拦截

--- src/main/java/com/zzj/core/annotation/RequestMapping.java
+++ src/main/java/com/zzj/core/annotation/RequestMapping.java
@@ -5,9 +5,10 @@ import com.zzj.core.constant.RequestMethod;
 import java.lang.annotation.*;
 
 @Documented
-@Target({ElementType.TYPE,ElementType.METHOD})
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RequestMapping {
     String value() default "";
-    RequestMethod[] method() default {RequestMethod.GET,RequestMethod.POST};
+
+    RequestMethod[] method() default {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT};
 }
